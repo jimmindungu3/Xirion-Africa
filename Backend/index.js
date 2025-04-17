53d8d2877e66d129be879ef29c736e0fda3f099c
james <jimmindungu3@gmail.com>
2025-04-17 13:20:13 +0300

    Allow port5174

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://xirion-africa.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://xirion-africa.vercel.app"],
     credentials: true,
   })
 );
