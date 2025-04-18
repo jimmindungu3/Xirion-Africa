3e9f2695b0cd3b11ecce83a0a24969c4d090c4a8
james <jimmindungu3@gmail.com>
2025-04-18 19:11:20 +0300

    dynamically render nav

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import SignIn from "./components/SignIn";
 import ProductUploader from "./components/ProductUploader";
 import Orders from "./components/Orders";
+import Nav from "./components/Nav";
+
+// Do not show Nav on sign in page
+const NavWrapper = () => {
+  const location = useLocation();
+  return location.pathname !== "/" && <Nav />;
+};
 
 const App = () => {
   return (
     <Router>
+      <NavWrapper />
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/upload-product" element={<ProductUploader />} />
