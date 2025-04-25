13799ef8492473cede56abff24994cb3af9192db
james <jimmindungu3@gmail.com>
2025-04-25 17:18:14 +0300

    fix different verification codes issue

--- Backend/services/sendCode.js
+++ Backend/services/sendCode.js
@@ -18,26 +18,25 @@ function generateVerificationCode(length = 6) {
   return code;
 }
 
-async function sendVerificationCode(clientEmail) {
+async function sendVerificationCode(email, verificationCode) {
   try {
-    const verificationCode = generateVerificationCode();
     const mailOptions = {
       from: `Xirion Africa <${process.env.MY_EMAIL}>`,
-      to: clientEmail,
+      to: email,
       subject: "Xirion Africa - Verify Your Email",
       html: `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
           <h2 style="text-align: center; color: #ff6600;">XIRION AFRICA</h2>
-          <p style="font-size: 16px; color: #555;">Thank you for signing up. To complete your registration, please use the verification code below:</p>
-          <div style="font-size: 24px; font-weight: bold; color: #FF6600; text-align: center; margin: 20px 0;">${verificationCode}</div>
+          <p style="font-size: 16px; color: #555;">Thank you for signing up. To complete your registration, use the verification code below:</p>
+          <div style="font-size: 16px; font-weight: bold; color: #FF6600; text-align: center; margin: 20px 0;">${verificationCode}</div>
           <p style="font-size: 16px; color: #555;">This code is valid for <strong>30 minutes</strong>. If you did not request this, please ignore this email.</p>
           
-          <hr style="margin: 30px 0;">
+          <hr style="margin-top: 30px; margin-bottom: 0;">
 
           <div style="font-size: 14px; color: #888; text-align: center;">
             <p style="margin: 0;">Why shop with us?</p>
             <p style="margin: 4px 0;">Affordable quality. Ethical sourcing. Fast shipping. Easy returns.</p>
-            <p style="margin: 4px 0;">Follow us: 
+            <p style="margin-top: 12px 0;">Follow Us: 
               <a href="#" style="text-decoration: none; color: #3b5998;">Facebook</a> | 
               <a href="#" style="text-decoration: none; color: #000;">TikTok</a> | 
               <a href="#" style="text-decoration: none; color: #C13584;">Instagram</a> | 
@@ -48,9 +47,8 @@ async function sendVerificationCode(clientEmail) {
         </div>
       `,
     };
-
-    const info = await transporter.sendMail(mailOptions);
-    return verificationCode;
+    const transporterResponse = await transporter.sendMail(mailOptions);
+    return transporterResponse;
   } catch (error) {
     console.error("Error sending email:", error);
     throw error;
